1586b95d733d4ad94efdd44e450a157cf6091c66
Dylan <nitrocodetech@gmail.com>
2024-01-30 06:59:34 -0500

    added url

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
 # from .restapis import related methods
-from .restapis import get_dealers_from_cf
+from .restapis import get_dealers_from_cf,get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -90,7 +90,11 @@ def get_dealerships(request):
         return HttpResponse(dealer_names)
 
 def get_dealer_details(request, dealer_id):
-    
+    if request.method == "GET":
+        url = ""
+        reviews = get_dealer_reviews_from_cf(url,dealer_id)
+        return HttpResponse(reviews)
+
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
